d23505358d425a4f2a01ed8800ec51ef063e76ba
sedsajjad313 <79037216+sedsajjad313@users.noreply.github.com>
2026-04-29 01:17:40 +0330

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+#I'm Smiling to the world.
 export const config = { runtime: "edge" };
 
 const TARGET_BASE = (process.env.TARGET_DOMAIN || "").replace(/\/$/, "");
@@ -60,3 +61,4 @@ export default async function handler(req) {
     return new Response("Bad Gateway: Tunnel Failed", { status: 502 });
   }
 }
+#Nothing is forbidden for me.
